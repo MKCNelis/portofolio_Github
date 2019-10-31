0393cc1b79751fdd36ab23fac02e836c384e3e27
mkcnelis <maikel.nelis@gmail.com>
2019-10-31 10:49:06 +0100

    java works

---
+++ JavaSript_files/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', 
+    function(){
+      Typed.new('.entry', {
+        strings: ["Portofolio Pierre", "Do you feel like giving up?", "Don't stop! keep going.", "You will get there."],
+        typeSpeed: 1
+      });
+  });
+  
