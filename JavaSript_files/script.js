2cad05213aa7b12967c555afad8f7fc931d0c226
Damien1304 <56149776+Damien1304@users.noreply.github.com>
2019-11-01 09:31:05 +0100

    Making an copy

---
+++ JavaSript_files/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', 
+    function(){
+      Typed.new('.entry', {
+        strings: ["Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel.", "You will get there."],
+        typeSpeed: 1
+      });
+  });
+  
