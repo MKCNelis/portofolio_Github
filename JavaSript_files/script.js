ab67ac11cf5732d9594517147af432c3894317b8
mkcnelis <maikel.nelis@gmail.com>
2019-11-01 09:20:07 +0100

    comitt updates

--- JavaSript_files/script.js
+++ JavaSript_files/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', 
     function(){
       Typed.new('.entry', {
-        strings: ["Portofolio Pierre", "Do you feel like giving up?", "Don't stop! keep going.", "You will get there."],
+        strings: ["Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel.", "You will get there."],
         typeSpeed: 1
       });
   });
