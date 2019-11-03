f7f43650bf00ba5e62aee615d05e605aeb19cbdb
mkcnelis <maikel.nelis@gmail.com>
2019-11-03 13:41:06 +0100

    update

--- JavaSript_files/script.js
+++ JavaSript_files/script.js
@@ -1,8 +1,10 @@
 document.addEventListener('DOMContentLoaded', 
     function(){
       Typed.new('.entry', {
-        strings: ["Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel.", "You will get there."],
+        strings: ["Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel.", "You will get there.","Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel."],
         typeSpeed: 1
       });
   });
   
+
+
