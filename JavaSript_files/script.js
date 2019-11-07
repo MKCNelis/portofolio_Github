931c0c4f20de2784fb2779f703fccb0fef48dfe5
Pierre Reumkens <pierre.spyrofan@gmail.com>
2019-11-07 07:59:54 +0100

    changed the menu to work and look right and the same on every page, shortened the changing text, put the menu setup into the stylesheet and fixe up small mistakes in the stylesheet

--- JavaSript_files/script.js
+++ JavaSript_files/script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', 
     function(){
       Typed.new('.entry', {
-        strings: ["Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel.", "You will get there.","Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel."],
+        strings: ["Portofolio Pierre", "Portofolio Damiën", "Portofolio Maikel.", "Portfolio"],
         typeSpeed: 1
       });
   });
